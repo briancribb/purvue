fae290545ac755b5d701530a0d001d9216d6fbe9
Brian Cribb <brian.cribb@gmail.com>
2018-01-12 16:02:42 -0500

    Playing around.

---
+++ cards/scripts/components.js
@@ -0,0 +1,9 @@
+Vue.component('individual-comment', {
+	template: '#comment-template',
+	props: ['commentpost']
+});
+
+Vue.component('dash-card', {
+	template: '#dash-card-template',
+	props: ['text']
+});
