0df22f26321ed2f5379ba3a6eb6590fe94eca48b
Brian Cribb <brian.cribb@gmail.com>
2018-01-12 16:44:49 -0500

    More stuff.

--- cards/scripts/index.js
+++ cards/scripts/index.js
@@ -18,6 +18,36 @@ new Vue({
 				author: 'Geoff Graham',
 				authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
 			}
+		],
+		IP: [
+			{ 
+				IP: '123.45.678.9',
+				quantity: '13'
+			},
+			{ 
+				IP: '234.56.789.0',
+				quantity: '8'
+			},
+			{ 
+				IP: '345.67.890.1',
+				quantity: '4'
+			},
+			{ 
+				IP: '456.78.901.2',
+				quantity: '12'
+			},
+			{ 
+				IP: '567.89.012.3',
+				quantity: '13'
+			},
+			{ 
+				IP: '678.90.123.4',
+				quantity: '2'
+			},
+			{ 
+				IP: '789.01.234.5',
+				quantity: '1'
+			}
 		]
 	},
 	methods: {
