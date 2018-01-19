765f4360e0cf3f45d82f0c839eb77260d282af79
Brian Cribb <brian.cribb@gmail.com>
2018-01-19 12:54:04 -0500

    Starting to play with Vuex.

--- cards/scripts/index.js
+++ cards/scripts/index.js
@@ -5,47 +5,9 @@ new Vue({
 	el: '#app',
 	data() {
 		return {
-			arrIP: [
-				{ 
-					number:		'123.45.678.9',
-					quantity:	13
-				},
-				{ 
-					number:		'234.56.789.0',
-					quantity:	8
-				},
-				{ 
-					number:		'345.67.890.1',
-					quantity:	4
-				},
-				{ 
-					number:		'456.78.901.2',
-					quantity:	12
-				},
-				{ 
-					number:		'567.89.012.3',
-					quantity:	13
-				},
-				{ 
-					number:		'678.90.123.4',
-					quantity:	2
-				},
-				{ 
-					number:		'789.01.234.5',
-					quantity:	1
-				}
-			]
 		}
 	},
 	methods: {
-		increment: function () {
-			//console.log(['increment()', this]);
-			this.arrIP[0].quantity ++;
-		},
-		decrement: function () {
-			//console.log(['decrement()', this]);
-			this.arrIP[0].quantity --;
-		},
 		addComment: function () {
 			const newCommentObj = {
 				text: this.newComment,
---
+++ cards/scripts/store.js
@@ -0,0 +1,47 @@
+// Make sure to call Vue.use(Vuex) first if using a module system
+
+Vue.use(Vuex);
+const store = new Vuex.Store({
+	state: {
+		arrIP: [
+			{ 
+				number:		'123.45.678.9',
+				quantity:	13
+			},
+			{ 
+				number:		'234.56.789.0',
+				quantity:	8
+			},
+			{ 
+				number:		'345.67.890.1',
+				quantity:	4
+			},
+			{ 
+				number:		'456.78.901.2',
+				quantity:	12
+			},
+			{ 
+				number:		'567.89.012.3',
+				quantity:	13
+			},
+			{ 
+				number:		'678.90.123.4',
+				quantity:	2
+			},
+			{ 
+				number:		'789.01.234.5',
+				quantity:	1
+			}
+		]
+	},
+	mutations: {
+		increment: function () {
+			//console.log(['increment()', this]);
+			state.arrIP[0].quantity ++;
+		},
+		decrement: function () {
+			//console.log(['decrement()', this]);
+			state.arrIP[0].quantity --;
+		}
+	}
+});
